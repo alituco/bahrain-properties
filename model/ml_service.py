1a7f4fb066ae9d502172b3208d7e57e239e6564d
Ali Altaraif <alituco@Alis-Laptop.local>
2025-02-24 20:36:51 -0700

    Updated port

--- model/ml_service.py
+++ model/ml_service.py
@@ -386,6 +386,6 @@ def fetch_parcel_endpoint():
 # RUN THE FLASK SERVICE
 # -----------------------------------------------------------------------
 if __name__ == "__main__":
-    port = int(os.getenv("FLASK_PORT", 5001))
+    port = int(os.getenv("FLASK_PORT", 5000))
     debug_mode = os.getenv("FLASK_DEBUG", "True").lower() == "true"
     app.run(host="0.0.0.0", port=port, debug=debug_mode)
