3322040f3fe1ea9372d28496f2381d07c82f42ab
Ali Altaraif <alituco@Alis-Laptop.local>
2025-03-29 00:01:55 -0700

    logging email and host for debugging

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -32,6 +32,9 @@ const transporter = nodemailer.createTransport({
 // Login Endpoint
 export const login: RequestHandler = async (req, res) => {
   const { email, password } = req.body;
+
+  console.log("LOGGING:", process.env.EMAIL_HOST, process.env.EMAIL_PORT, process.env.EMAIL_USER);
+
   try {
     const userResult = await pool.query(
       'SELECT user_id, email, password FROM users WHERE email = $1 LIMIT 1',
