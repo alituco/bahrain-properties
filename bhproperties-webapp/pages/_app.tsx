fed47b5c3c912467750565535f6867695c4b16fd
Ali Altaraif <alituco@Alis-Laptop.local>
2025-05-09 17:46:49 -0700

    explicit font setting.

--- bhproperties-webapp/pages/_app.tsx
+++ bhproperties-webapp/pages/_app.tsx
@@ -1,22 +1,23 @@
-import '../styles/globals.scss';
+import '../styles/globals.scss';                           
+import '../public/assets/icon-fonts/remixicon/fonts/remixicon.css';   
+
 import ContentLayout from '@/shared/layouts-components/layout/content-layout';
 import Authenticationlayout from '@/shared/layouts-components/layout/authentication-layout';
 
-const layouts:any = {
-
+const layouts: Record<string, React.FC<any>> = {
   ContentLayout: ContentLayout,
   Authenticationlayout: Authenticationlayout,
-
 };
-function MyApp({ Component, pageProps }:any) {
 
-  const Layout = layouts[Component.layout] || ((pageProps:any ) => <Component>{pageProps}</Component>);
+function MyApp({ Component, pageProps }: any) {
+  const Layout = layouts[Component.layout] ||
+                 (({ children }: any) => <>{children}</>);
 
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
 
 export default MyApp;
