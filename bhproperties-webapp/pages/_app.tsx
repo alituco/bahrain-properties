85050f307eedd2f0eff52225660a595d9c2bfef1
Ali Altaraif <alituco@Alis-Laptop.local>
2025-05-17 16:20:28 -0700

    changed directory of babel folder

--- bhproperties-webapp/pages/_app.tsx
+++ bhproperties-webapp/pages/_app.tsx
@@ -1,4 +1,11 @@
-import '../styles/globals.scss';                           
+import '../styles/globals.scss';
+
+import { Poppins } from 'next/font/google';               
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  display: 'swap',
+});
 
 import ContentLayout from '@/shared/layouts-components/layout/content-layout';
 import Authenticationlayout from '@/shared/layouts-components/layout/authentication-layout';
@@ -9,13 +16,16 @@ const layouts: Record<string, React.FC<any>> = {
 };
 
 function MyApp({ Component, pageProps }: any) {
-  const Layout = layouts[Component.layout] ||
-                 (({ children }: any) => <>{children}</>);
+  const Layout =
+    layouts[(Component as any).layout] ||
+    (({ children }: any) => <>{children}</>);
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div className={poppins.className}>                   
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   );
 }
 
